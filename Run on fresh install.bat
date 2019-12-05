910ea2899c372423d40f14c04a94150a281cc2a3
Risingfeanyx <35235934+Risingfeanyx@users.noreply.github.com>
2019-12-05 05:24:02 -0500

    Update Run on fresh install.bat

--- Run on fresh install.bat
+++ Run on fresh install.bat
@@ -8,12 +8,14 @@ TITLE Install Common Stuff
 
 echo " Install chocolatey Chocolatey is a package manager for Windows (like apt-get or yum but for Windows).  It was designed to be a decentralized framework for quickly installing applications and tools that you need. It is built on the NuGet infrastructure currently using PowerShell as its focus for delivering packages from the distros to your door, err computer."
 
-echo "Create a restore point, just in case things go haywire"
+echo "Create a restore point, just in case things go haywire. Doesn't actually do this for you"
 start /WAIT systempropertiesprotection
 
 echo "Clean up Junk files"
 #cleanmgr.exe /cdrive
 
+
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+#installs chocolatey.org. "Chocolatey is a software management solution unlike anything else you've ever experienced on Windows. Chocolatey brings the concepts of true package management to allow you to version things, manage dependencies and installation order, better inventory management, and other features."
 
 start Common_Install_Part_Deux.bat
